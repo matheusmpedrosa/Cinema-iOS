c11c56bb4669d88cbd8661c7bf2f84645f6e4fa5
Matheus Pedrosa <math.pedrosa@hotmail.com>
2020-01-19 04:13:00 -0300

    tableview

--- Cinema-iOS/ViewController.swift
+++ Cinema-iOS/ViewController.swift
@@ -8,9 +8,24 @@
 
 import UIKit
 
+enum TableViewSections: Int {
+    case nowPlaying = 0
+    case upcoming = 1
+    case popular = 2
+}
+
 class ViewController: UIViewController {
     
     private var menu = MenuView(frame: .zero)
+    lazy private var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.alwaysBounceVertical = false
+        tableView.backgroundColor = .clear
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +36,49 @@ class ViewController: UIViewController {
     private func setupView() {
         buildViewHierarchy()
         setupConstraints()
+        overrideUserInterfaceStyle()
         self.isAccessibilityElement = false
     }
     
     private func buildViewHierarchy() {
         view.addSubview(menu)
+        view.addSubview(tableView)
     }
     
     private func setupConstraints() {
         menu.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 0, bottom: nil, paddingBottom: 0, left: view.safeAreaLayoutGuide.leftAnchor, paddingLeft: 20, right: view.safeAreaLayoutGuide.rightAnchor, paddingRight: 20, width: 0, height: 70)
+        tableView.anchor(top: menu.bottomAnchor, paddingTop: 10, bottom: view.bottomAnchor, paddingBottom: 0, left: view.leftAnchor, paddingLeft: 0, right: view.rightAnchor, paddingRight: 0, width: 0, height: 0)
+    }
+    
+    private func overrideUserInterfaceStyle() {
+        overrideUserInterfaceStyle = .dark
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.selectionStyle = .none
+        cell.textLabel?.setRegularFont()
+        cell.backgroundColor = .clear
+        switch indexPath.section {
+        case TableViewSections.nowPlaying.rawValue:
+            cell.textLabel?.text = "Now Playing"
+        case TableViewSections.upcoming.rawValue:
+            cell.textLabel?.text = "Upcoming"
+        case TableViewSections.popular.rawValue:
+            cell.textLabel?.text = "Popular"
+        default:
+            cell.textLabel?.text = "DEFAULT"
+        }
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+}
